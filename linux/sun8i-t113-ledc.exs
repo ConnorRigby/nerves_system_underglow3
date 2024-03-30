c35e7d2bfe482e8296605b2ef7e908292a7fd031
Connor Rigby <connorr@hey.com>
2024-03-29 19:41:29 -0600

    Bringup updates
    * Enable ledc
    * Enable temporary rtl wifi driver
    * Enable kernel bluetooth
    * Enable ipv6
    
    Signed-off-by: Connor Rigby <connorr@hey.com>

--- linux/sun8i-t113-ledc.exs
+++ linux/sun8i-t113-ledc.exs
@@ -1,3 +1,3 @@
 evaluated = EEx.eval_file("../../nerves_system_underglow3/linux/sun8i-t113-ledc.dtsi.eex")
 File.write("../../nerves_system_underglow3/linux/sun8i-t113-ledc.dtsi", evaluated)
-File.cp!("../../nerves_system_underglow3/linux/sun8i-t113-ledc.dtsi", "build/linux-6.1.24/arch/arm/boot/dts/sun8i-t113-ledc.dtsi")
+File.cp!("../../nerves_system_underglow3/linux/sun8i-t113-ledc.dtsi", "build/linux-6.7.10/arch/arm/boot/dts/sun8i-t113-ledc.dtsi")
